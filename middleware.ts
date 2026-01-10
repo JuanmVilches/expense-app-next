e61dea96687ee2c3026b3488a134570d821cf47f
Juan Manuel Vilches <juanmanuelvilches99@gmail.com>
2026-01-10 19:47:13 -0300

    commented middleware

--- middleware.ts
+++ middleware.ts
@@ -31,6 +31,6 @@ export async function middleware(request: NextRequest) {
   }
 }
 
-export const config = {
-  matcher: ["/form", "/list", "/dashboard"],
-};
+// export const config = {
+//   matcher: ["/form", "/list", "/dashboard"],
+// };
